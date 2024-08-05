87ef60bf8e3280717ca55f14d69f15f9be2fa61b
zohidbe <zohaxaydarov@gmail.com>
2024-08-06 00:25:00 +0500

    homwork end

---
+++ main.js
@@ -0,0 +1,61 @@
+// For. k va n butun sonlari berilgan (n > 0). k sonini n marta chiqaruvchi programma
+
+// task 1
+
+// let k = parseInt(prompt("k sonini kiriting"));
+
+// let n = parseInt(prompt("n sonini kiriting"));
+
+// for (let i = 0; i < n ; i++) {
+//     console.log(k);
+// }
+
+// task 2//
+
+// let a = parseInt(prompt("a sonini kiriting"));
+// let b = parseInt(prompt ("b sonini kiriting"));
+
+// let count = 0;
+
+// for( let i = a; i <= b; i++){
+//     console.log(i);
+//     count++;
+// }
+// console.log("count");
+
+// task 3 
+
+// let pricePerKg = parseFloat(prompt("Bir kg konfetning narxini kiriting: "));
+
+// for (let i = 1; i <= 10; i++) {
+//     let weight = i / 10;
+//     console.log(weight + " kg konfet narxi: " + (pricePerKg * weight));
+// }
+
+// task 5 
+
+// let pricePerKg = parseFloat(prompt("Bir kg konfetning narxini kiriting: "));
+
+// for (let i = 12; i <= 20; i += 2) {
+//     let weight = i / 10;
+//     console.log(weight + " kg konfet narxi: " + (pricePerKg * weight));
+// }
+
+
+
+/////////////// boolen oyit misolar   
+
+// task 1 
+
+// let A = parseInt(prompt("A sonini kiriting: "));
+// let B = parseInt(prompt("B sonini kiriting: "));
+// let condition = (A % 2 !== 0 && B % 2 === 0) || (A % 2 === 0 && B % 2 !== 0);
+// console.log("A va B sonlarining faqat bittasi toq son:", condition);
+
+//task 2// 
+
+
+// let A = parseInt(prompt("A sonini kiriting: "));
+// let B = parseInt(prompt("B sonini kiriting: "));
+// let condition = A % 2 !== 0 && B % 2 !== 0;
+// console.log("A va B sonlari toq sonlar:", condition);
